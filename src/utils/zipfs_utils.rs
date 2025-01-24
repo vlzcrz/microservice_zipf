cee31942a43ca0eb3b455f8d106a13d8b9231d9e
vlzcrz <velizcruzv@gmail.com>
2025-01-23 23:44:17 -0300

    feat: Added response top50 words and quantities

--- src/utils/zipfs_utils.rs
+++ src/utils/zipfs_utils.rs
@@ -24,6 +24,10 @@ pub struct ZipfResponse {
     vector_keys: Vec<String>,
     vector_values: Vec<f64>,
     vector_ranking: Vec<f64>,
+    words_trend_n50: Vec<String>,
+    values_trend_n50: Vec<u32>,
+    total_words: u32,
+    total_different_words: u32,
 }
 
 pub async fn zipf_law_process(content: String, file_path: &str) -> Result<ZipfResponse, Error> {
@@ -51,16 +55,48 @@ pub async fn zipf_law_process(content: String, file_path: &str) -> Result<ZipfRe
 
     keys.reverse();
     values.reverse();
+
     let capacity = values.len() as u32;
+    let total: u32 = values.iter().sum();
     let ranking: Vec<u32> = (1..=capacity).collect();
+
+    let mut words_first_n50: Vec<String> = Vec::with_capacity(50);
+    let mut values_first_n50: Vec<u32> = Vec::with_capacity(50);
+
+    if capacity < 50 {
+        for word in keys.iter().take(capacity as usize) {
+            words_first_n50.push(word.to_string());
+        }
+        for value in values.iter().take(capacity as usize) {
+            values_first_n50.push(*value);
+        }
+    } else {
+        for words in keys.iter().take(50) {
+            words_first_n50.push(words.to_string());
+        }
+        for value in values.iter().take(50) {
+            values_first_n50.push(*value);
+        }
+    }
 
     // Aplicamos log base 10 para graficar asimilando una recta con pendiente negativa
     let log_values: Vec<f64> = values.iter().map(|&val| (val as f64).log10()).collect();
     let log_ranking: Vec<f64> = ranking.iter().map(|&val| (val as f64).log10()).collect();
 
+    println!("{:?}", keys);
+    println!("{:?}", log_ranking);
+    println!("{:?}", log_values);
+    println!("{:?}", words_first_n50);
+    println!("{:?}", total);
+    println!("{:?}", capacity);
+
     Ok(ZipfResponse {
         vector_keys: keys,
         vector_values: log_values,
         vector_ranking: log_ranking,
+        words_trend_n50: words_first_n50,
+        values_trend_n50: values_first_n50,
+        total_words: total,
+        total_different_words: capacity,
     })
 }
